bd9baae3efc9bb8ccaa62f7a60c5b33609844edc
Alejandro <agoal0@gmail.com>
2024-01-16 14:44:37 +0100

    creacion de cruds y relaciones

---
+++ app/Models/Comments.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Comments extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function post():BelongsTo {
+        return $this->belongsTo(Post::class);
+    }
+}
